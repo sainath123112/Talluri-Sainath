64895749080ae23099e695992fc61140c21dc78b
sainath123112 <113941618+sainath123112@users.noreply.github.com>
2023-03-17 18:59:24 -0500

    deploy website

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -3,6 +3,7 @@ import Logos from "../logos/Logos.js";
 import Todoapp from "./images/todo_app.mp4";
 import Quotegenerator from "./images/quote_generator.mp4";
 import FoodOrderApp from "./images/foodorderapp.mp4";
+import MultiApiApp from "./images/multi_api_app.mp4";
 
 function Projects() {
   function disableRightClick(e) {
@@ -82,7 +83,31 @@ function Projects() {
       <a href="https://github.com/sainath123112/food-order-app" className="font-bold" target="__blank">Source Code</a>
     </div>
         </div>
-        <div className="project-item"></div>
+
+        <div className="flex flex-col w-[350px] bg-[#fffffe] h-auto items-center m-4 border-none rounded-tr-2xl rounded-bl-2xl shadow">
+          <video
+            width="100%"
+            autoPlay
+            loop
+            muted
+            onContextMenu={disableRightClick}
+            className="border-none rounded-tr-2xl"
+          >
+            <source src={MultiApiApp} type="video/mp4" />
+          </video>
+          <h2 className="text-2xl font-bold tracking-[1px] py-2 uppercase">Quote Fact Joke Riddle App</h2>
+          <ul className="project-list">
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JAVASCRIPT</li>
+            <li>REACT</li>
+            <li>REST API</li>
+          </ul>  
+          <div className="project-item-buttons pb-3 pt-2">
+      <a href="https://sainath123112.github.io/Multiple-Api-App/" className="font-bold" target="__blank">Live Demo</a>
+      <a href="https://github.com/sainath123112/Multiple-Api-App" className="font-bold" target="__blank">Source Code</a>
+    </div>
+        </div>
         <div className="project-item"></div>
         <div className="project-item"></div>
         <div className="project-item"></div>
