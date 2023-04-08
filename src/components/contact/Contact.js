9e22bafcd758a5dc09250984cad58069d9d6fc8e
sainath123112 <113941618+sainath123112@users.noreply.github.com>
2023-04-08 16:12:49 -0500

    deploy website

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,33 +1,41 @@
 import "./Contact.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef } from "react";
 import { useLocation } from "react-router-dom";
 import Logos from "../logos/Logos.js";
 import ContactLinks from "../contact-links/ContactLinks";
 import {motion} from "framer-motion"
+import emailjs from "@emailjs/browser"
 
 function Contact({setcomponentpath})
 {
-    const [to, setTo] = useState('');
-  const [subject, setSubject] = useState('');
-  const [message, setMessage] = useState('');
-
+    const form = useRef();
     const location = useLocation();
     useEffect(() => {
         setcomponentpath(location.pathname);
     }, [setcomponentpath, location.pathname])
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs.sendForm('service_a09o4gi', 'template_yxe6esk', form.current, '84jnRCA9o4EPeE1mm')
+          .then((result) => {
+            alert("Email sent successfully..!")
+          }, (error) => {
+              alert(error.text);
+          });
+      };
 
     return( 
         <motion.div animate={{x:0}} initial={{x: 1600}} className="contact"> 
             <h1>Contact</h1>
             <p>Send me an E-mail to hire..!</p>
             <div className="contact-form">
-                <form>
+                <form ref={form} onSubmit={sendEmail}>
                     <div>
-                    <input type="text" name="name" placeholder="Name"></input>
-                    <input type="email" value={to} name="email" onChange={(event) => setTo(event.target.value)} placeholder="Email"></input>
+                    <input type="text" name="user_name" placeholder="Name" required></input>
+                    <input type="email" name="user_email" placeholder="Email" required></input>
                     </div>
-                    <input type="text" name="subject" value={subject} onChange={(event) => setSubject(event.target.value)} placeholder="Subject"></input>
-                    <textarea type="textarea" name="message" value={message} onChange={(event) => setMessage(event.target.value)} placeholder="Message"></textarea>
+                    <input type="text" name="user_subject" placeholder="Subject" required></input>
+                    <textarea type="textarea" name="message" placeholder="Message" required></textarea>
                     <button type="submit" >Send</button>
                 </form>
             </div>
